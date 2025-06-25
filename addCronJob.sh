554f4f27b805567ca6b77d86b3239307413d0032
matthewstuartedwards <43154341+matthewstuartedwards@users.noreply.github.com>
2025-06-25 09:13:06 -0600

    Update addCronJob.sh
    The @reboot didn't work for cron updating due to not having network yet.  So I created a systemd service to launch the telegram bot when network is available.  This lets you get your IP address after a reboot.

--- addCronJob.sh
+++ addCronJob.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 (crontab -l 2>/dev/null; echo "0 0 6 * * PYTHONPATH=<MY PYTHONPATH. eg.: ~/miniconda3/lib/python3.12/site-packages> /storage/gitRepos/TelegramIPLogger/logIPToTelegram.py") | crontab -
-(crontab -l 2>/dev/null; echo "@reboot PYTHONPATH=<MY PYTHONPATH. eg.: ~/miniconda3/lib/python3.12/site-packages> /storage/gitRepos/TelegramIPLogger/logIPToTelegram.py") | crontab -
+
+sudo cp telegramStartup.service /etc/systemd/system
+sudo systemctl enable telegramStartup.service
